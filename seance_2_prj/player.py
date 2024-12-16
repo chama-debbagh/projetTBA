1d6b40bc3b55c3d3a0ec179e40735bced88e99f1
chama-debbagh <chama.debbagh@edu.esiee.fr>
2024-12-16 15:47:27 +0000

    seance3

--- seance_2_prj/player.py
+++ seance_2_prj/player.py
@@ -7,6 +7,17 @@ class Player():
         self.current_room = None
         self.history=[] #initialisation de l historique avec une liste videee
     
+
+    def get_history(self):
+        """ retourner une representation des rooms qu'on a visiter. mais n'inclue pas la room actuelle
+        """
+        if not self.history:  #s'il y a rien dans l'historique
+           return ""
+        history_string="\nVous avez deja visiter les pieces suivantes:"
+        for room in self.history :
+            history_string+= f"\n    -{room.description} "
+        return history_string
+    
     
     # Define the move method.
     def move(self, direction):
@@ -22,6 +33,7 @@ class Player():
         # Set the current room to the next room.
         self.current_room = next_room
         print(self.current_room.get_long_description())
+        print(self.get_history())
         return True
 
     
