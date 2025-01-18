8eb36d848ec2798bf4c494656ba1ce8a40685d76
chama-debbagh <chama.debbagh@edu.esiee.fr>
2025-01-18 22:02:15 +0000

    codes

--- CodesProjetTBA/item.py
+++ CodesProjetTBA/item.py
@@ -2,7 +2,7 @@ class Item:
     def __init__(self, name, description, weight):
         self.name = name
         self.description = description
-        self.weight = weight
+       # self.weight = weight
 
-    def __str__(self):
-        return f"{self.name}: {self.description} ({self.weight} kg)"
+   # def __str__(self):
+      #  return f"{self.name}: {self.description} ({self.weight} kg)"
