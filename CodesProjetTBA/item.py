3e8f30326c6b8fa0beac2e276d23bb6925e3a1b0
chama-debbagh <chama.debbagh@edu.esiee.fr>
2025-01-18 22:10:11 +0100

    Delete CodesProjetTBA/item.py

--- CodesProjetTBA/item.py
+++
@@ -1,8 +0,0 @@
-class Item:
-    def __init__(self, name, description, weight):
-        self.name = name
-        self.description = description
-        self.weight = weight
-
-    def __str__(self):
-        return f"{self.name}: {self.description} ({self.weight} kg)"
